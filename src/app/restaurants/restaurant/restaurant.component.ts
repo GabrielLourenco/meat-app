3c0d8a7ce818b6ce19c87f43f238f93c08c818f3
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-10-04 17:17:46 -0300

    Animando os componentes de restaurantes, itens do menu e do carrinho

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Restaurant } from '../../models/restaurant.model';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('restaurantAppeared', [
+      state('ready', style({opacity:1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-20px)'}),
+        animate('300ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
 
   @Input() restaurant: Restaurant
+
+  public restaurantState: string = 'ready'
 
   constructor() { }
 
